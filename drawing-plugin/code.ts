7d3ea0f22ec214a497c727a8f92dec2fecce33c1
Ryan Fitzgerald <rwfitzge@gmail.com>
2022-11-02 12:08:17 -0700

    Don't recreate vector on every frame

--- drawing-plugin/code.ts
+++ drawing-plugin/code.ts
@@ -53,36 +53,31 @@ figma.ui.onmessage = (msg: Message) => {
   if (msg.type === "line") {
     let vector;
 
-    // If we previously created a vector for the same line, delete it
+    // If we previously created a vector for the same line, update it
     if (lineIdMap[msg.id] != null) {
       vector = figma.getNodeById(lineIdMap[msg.id]) as VectorNode;
-      vector.remove();
+    } else {
+      vector = figma.createVector();
+      vector.strokeWeight = strokeWeight;
+      vector.strokeCap = "ROUND";
+      vector.strokes = [
+        {
+          type: "SOLID",
+          color: { r: 1, g: 0, b: 0 },
+        },
+      ];
+      lineIdMap[msg.id] = vector.id;
     }
 
-    vector = figma.createVector();
     vector.relativeTransform = [
       [1, 0, bounds.x],
       [0, 1, bounds.y],
     ];
-    vector.strokeWeight = strokeWeight;
-    vector.strokeCap = "ROUND";
-    vector.strokes = [
-      {
-        type: "SOLID",
-        color: { r: 1, g: 0, b: 0 },
-      },
-    ];
-    lineIdMap[msg.id] = vector.id;
 
     const data =
       "M " +
       msg.points
-        .map(([x, y]) =>
-          [
-            Math.round(((640 - x) / 640) * bounds.width),
-            Math.round((y / 480) * bounds.height),
-          ].join(" ")
-        )
+        .map((point) => cameraPointToFigmaPoint(point).join(" "))
         .join(" L ");
 
     vector.vectorPaths = [
