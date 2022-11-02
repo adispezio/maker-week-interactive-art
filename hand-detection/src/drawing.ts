b4f3fbd37397337155155d79a4e1f8f70b6b4768
Ryan Fitzgerald <rwfitzge@gmail.com>
2022-11-02 15:07:35 -0700

    Add stable hand ids

--- hand-detection/src/drawing.ts
+++ hand-detection/src/drawing.ts
@@ -1,4 +1,16 @@
 import simplify from "./simplify";
+import sortBy from "lodash/sortBy";
+import range from "lodash/range";
+
+// What's the largest number of pixels we could expect a fingertip to move in
+// one frame?
+const MAX_PX_MOVED_PER_FRAME = 100;
+
+// How many frames is a hand allowed to disappear for before coming back?
+const MAX_HAND_FRAME_GAP = 10;
+
+// How many frames is a hand allowed to not point before we cut off the line?
+const MAX_LINE_FRAME_GAP = 5;
 
 type KeyPoint = {
   // Coords are screen pixels, x in 0..640 and y in 0..480
@@ -15,7 +27,7 @@ type KeyPoint3D = {
   name: string;
 };
 
-type Hand = {
+type TFHand = {
   keypoints: Array<KeyPoint>;
   keypoints3D: Array<KeyPoint3D>;
   score: number;
@@ -91,74 +103,171 @@ function chaikinSmooth(input: Array<[number, number]>) {
   return output;
 }
 
-// { [0]: null | { id: "...", coords: [[x, y], ...] } }
-let allLines = {};
-let prevHandCount = 0;
+type DrawingHand = {
+  id: number;
+  point: [number, number];
+  lastSeenFrameId: number;
+  line: {
+    id: number;
+    points: Array<[number, number]>;
+    lastSeenFrameId: number;
+  } | null;
+};
 
-export function handleFrame(hands: Array<Hand>, ctx: CanvasRenderingContext2D) {
-  hands = (hands || []).filter((hand) => Object.keys(hand).length > 0);
+let handsById: Record<number, DrawingHand> = {};
+let nextHandId = 0;
+let nextLineId = 0;
+let frameId = 0;
 
-  if (hands.length < prevHandCount) {
-    for (let handIndex = hands.length; handIndex < prevHandCount; handIndex++) {
-      delete allLines[handIndex];
+export function handleFrame(
+  tfHands: Array<TFHand>,
+  ctx: CanvasRenderingContext2D
+) {
+  frameId++;
+  tfHands = (tfHands || []).filter((tfHand) => Object.keys(tfHand).length > 0);
+
+  // First, map the hands we're getting from TensorFlow to our existing state.
+  // We can't trust the indices in the array to be stable, so we need to use a
+  // heuristic to determine which hand is which:
+  // - For each pair of old hand and new hand, calculate the distance between
+  //   their index finger tip locations.
+  // - Go through those pairs in order of ascending distance.
+  // - For each pair, if neither the old nor new hand are already taken, decide
+  //   if we think it's a valid mapping based on the distance and the last time
+  //   we saw the old hand. If it is valid, record it and remove both hands
+  //   from further consideration.
+  const handPairs = sortBy(
+    Object.entries(handsById).flatMap(([handId, oldHand]) =>
+      tfHands.map((newHand, newHandIdx) => {
+        const newHandLocation = newHand.keypoints[KeyPointID.IndexFingerTip];
+        return {
+          handId: Number(handId),
+          newHandIdx,
+          distance: distance(
+            { x: oldHand.point[0], y: oldHand.point[1] },
+            newHandLocation
+          ),
+          location: [newHandLocation.x, newHandLocation.y] as [number, number],
+        };
+      })
+    ),
+    (pair) => pair.distance
+  );
+  const newHandIdxByOldHandId: Record<number, number> = {};
+  const oldHandIdByNewHandIdx: Record<number, number> = {};
+  const removedHandIds = new Set(Object.keys(handsById).map(Number));
+  const newHandIndices = new Set(range(0, tfHands.length));
+
+  for (const { handId, newHandIdx, distance, location } of handPairs) {
+    if (newHandIdxByOldHandId[handId] != null) continue;
+    if (oldHandIdByNewHandIdx[newHandIdx] != null) continue;
+
+    const oldHand = handsById[handId];
+    const framesElapsed = frameId - oldHand.lastSeenFrameId;
+    if (distance > framesElapsed * MAX_PX_MOVED_PER_FRAME) {
+      continue;
+    }
+
+    newHandIdxByOldHandId[handId] = newHandIdx;
+    oldHandIdByNewHandIdx[newHandIdx] = handId;
+    removedHandIds.delete(handId);
+    newHandIndices.delete(newHandIdx);
+    oldHand.point = location;
+    oldHand.lastSeenFrameId = frameId;
+  }
+
+  // For each removed hand that's old enough, send a "gone" message.
+  for (const handId of removedHandIds) {
+    const oldHand = handsById[handId];
+    const framesElapsed = frameId - oldHand.lastSeenFrameId;
+
+    if (framesElapsed > MAX_HAND_FRAME_GAP) {
+      delete handsById[handId];
       ws.send(
         JSON.stringify({
           type: "presence",
-          id: handIndex,
+          id: handId,
           gone: true,
         })
       );
     }
   }
-  prevHandCount = hands.length;
 
-  for (const [handIndex, hand] of hands.entries()) {
-    if (hand.keypoints == null) {
-      allLines[handIndex] = null;
-      continue;
+  for (const [tfHandIndex, tfHand] of tfHands.entries()) {
+    const indexTip = tfHand.keypoints[KeyPointID.IndexFingerTip];
+    const indexDip3D = tfHand.keypoints3D[KeyPointID.IndexFingerDip];
+    const indexTip3D = tfHand.keypoints3D[KeyPointID.IndexFingerTip];
+    const middleTip3D = tfHand.keypoints3D[KeyPointID.MiddleFingerTip];
+
+    const oldHandId = oldHandIdByNewHandIdx[tfHandIndex];
+
+    let hand: DrawingHand;
+    if (oldHandId == null) {
+      const id = nextHandId++;
+      handsById[id] = {
+        id,
+        point: [indexTip.x, indexTip.y],
+        lastSeenFrameId: frameId,
+        line: null,
+      };
+      hand = handsById[id];
+    } else {
+      hand = handsById[oldHandId];
     }
 
-    const indexTip = hand.keypoints[KeyPointID.IndexFingerTip];
-
-    const indexDip3D = hand.keypoints3D[KeyPointID.IndexFingerDip];
-    const indexTip3D = hand.keypoints3D[KeyPointID.IndexFingerTip];
-    const middleTip3D = hand.keypoints3D[KeyPointID.MiddleFingerTip];
-
     // Regardless of whether they're gesturing, send a message containing the
-    // indexTip's location for presence purposes
+    // indexTip's location for presence purposes.
     ws.send(
-      // TODO: Add stable hand id
       JSON.stringify({
         type: "presence",
-        id: handIndex,
+        id: hand.id,
         point: [indexTip.x, indexTip.y],
       })
     );
 
-    if (
-      distance3D(indexTip3D, middleTip3D) <=
-      3 * distance(indexTip3D, indexDip3D)
-    ) {
-      allLines[handIndex] = null;
+    // TODO: Better gesture heuristic
+    const isPointing =
+      distance3D(indexTip3D, middleTip3D) >
+      3 * distance(indexTip3D, indexDip3D);
+
+    if (!isPointing) {
+      // If too many frames have elapsed since the last time we saw this hand
+      // pointing, remove the line so that the next time they point it will
+      // start a new one.
+      if (
+        hand.line &&
+        frameId - hand.line.lastSeenFrameId > MAX_LINE_FRAME_GAP
+      ) {
+        hand.line = null;
+      }
       continue;
     }
 
-    if (allLines[handIndex] == null) {
-      const id = Math.floor(Math.random() * 1000000);
-      allLines[handIndex] = { id, points: [] };
+    if (hand.line == null) {
+      const lineId = nextLineId++;
+      hand.line = {
+        id: lineId,
+        points: [],
+        lastSeenFrameId: frameId,
+      };
     }
-    const line = allLines[handIndex];
 
-    line.points.push([indexTip.x, indexTip.y]);
+    hand.line.points.push([indexTip.x, indexTip.y]);
+    hand.line.lastSeenFrameId = frameId;
 
-    let smoothedPoints = line.points;
+    let smoothedPoints = hand.line.points;
     smoothedPoints = simplify(smoothedPoints, 5, true);
     smoothedPoints = chaikinSmooth(smoothedPoints);
     smoothedPoints = chaikinSmooth(smoothedPoints);
 
     // Send message containing this line's updated geometry
     ws.send(
-      JSON.stringify({ type: "line", id: line.id, handId: handIndex, points: smoothedPoints })
+      JSON.stringify({
+        type: "line",
+        id: hand.line.id,
+        handId: hand.id,
+        points: smoothedPoints,
+      })
     );
 
     // Render debug visualization
